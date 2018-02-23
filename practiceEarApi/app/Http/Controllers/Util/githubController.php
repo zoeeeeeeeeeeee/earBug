65ef0778c6b144c46eb2310405fdf679f2f9c8a1
lizhe <lizhe@taptap.com>
2018-02-23 18:19:07 +0800

    test gitignore

--- practiceEarApi/app/Http/Controllers/Util/githubController.php
+++ practiceEarApi/app/Http/Controllers/Util/githubController.php
@@ -14,27 +14,29 @@ use App\Http\Controllers\Controller;
 class githubController extends Controller{
 
     public function pullRepo(Request $request){
-        $secret = 'earBugHasBeenReborn';
+//        $secret = 'earBugHasBeenReborn';
+//
+//        $headers = getallheaders();
+//        $hubSignature = $headers['X-Hub-Signature'];
+//
+//        // Split signature into algorithm and hash
+//        list($algo, $hash) = explode('=', $hubSignature, 2);
+//
+//        // Get payload
+//        $payload = file_get_contents('php://input');
+//
+//        // Calculate hash based on payload and the secret
+//        $payloadHash = hash_hmac($algo, $payload, $secret);
+//
+//        // Check if hashes are equivalent
+//        if ($hash !== $payloadHash) {
+//            // Kill the script or do something else here.
+//            die('Bad secret');
+//        }
 
-        $headers = getallheaders();
-        $hubSignature = $headers['X-Hub-Signature'];
+        system("cd /earBug/codeRepo/earBug >> /tmp/pull.log");
+        system("echo here >> /tmp/pull.log");
 
-        // Split signature into algorithm and hash
-        list($algo, $hash) = explode('=', $hubSignature, 2);
-
-        // Get payload
-        $payload = file_get_contents('php://input');
-
-        // Calculate hash based on payload and the secret
-        $payloadHash = hash_hmac($algo, $payload, $secret);
-
-        // Check if hashes are equivalent
-        if ($hash !== $payloadHash) {
-            // Kill the script or do something else here.
-            die('Bad secret');
-        }
-
-        system("cd /earBug/codeRepo/earBug");
-        system("/usr/bin/git pull origin master");
+        system("/usr/bin/git pull origin master >> /tmp/pull.log");
     }
 }
